d32692b03f5637fcf70de6358947b4baacda30a4
aayush598 <aayushgid598@gmail.com>
2025-03-08 22:19:20 +0530

    Procfile added

--- server/main.py
+++ server/main.py
@@ -4,3 +4,6 @@ app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
 @app.route('/')
 def hello():
     return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
